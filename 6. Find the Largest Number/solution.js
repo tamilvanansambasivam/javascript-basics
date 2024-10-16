db5584e016936b6c0b63496faac1deb41f060f42
tamilvanan <tamilvanansambasivam@gmail.com>
2024-10-16 08:19:56 +0530

    feat: add findLargest function

---
+++ 6. Find the Largest Number/solution.js
@@ -0,0 +1,7 @@
+// Write a function that takes an array of numbers and returns the largest one.
+
+function findLargest(arr) {
+  return Math.max(...arr);
+}
+
+console.log(findLargest([2, 4, 9, 1])); // Output: 9
